fa950c4ddfeb20ee5aa61264477e540676c76dbd
Scott Prigmore <prig6030@stthomas.edu>
2020-10-05 20:09:34 -0500

    Green - fixed roll

--- SkunkProject/test/skunk/domain/PredictableDie.java
+++ SkunkProject/test/skunk/domain/PredictableDie.java
@@ -5,7 +5,7 @@ public class PredictableDie
 	private int[] theRolls;
 	private int nextInt;
 	private int lastRoll;
-	
+
 	public PredictableDie()
 	{
 
@@ -14,18 +14,26 @@ public class PredictableDie
 	public PredictableDie(int[] is)
 	{
 		this.theRolls = is;
-		this.nextInt = 0 ;
+		this.nextInt = 0;
 	}
 
 	public void roll()
 	{
 		this.lastRoll = theRolls[this.nextInt];
-		
+		if (this.nextInt == 2)
+		{
+			this.nextInt = 0;
+		}
+		else
+		{
+			this.nextInt++;
+		}
+
 	}
 
 	public int getLastRoll()
 	{
-		
+
 		return this.lastRoll;
 	}
 }

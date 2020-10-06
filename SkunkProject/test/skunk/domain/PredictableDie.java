cd706ee59728861100110050c7d965c8f9be2d11
Scott Prigmore <prig6030@stthomas.edu>
2020-10-05 19:58:40 -0500

    Red - First failing test

---
+++ SkunkProject/test/skunk/domain/PredictableDie.java
@@ -0,0 +1,26 @@
+package skunk.domain;
+
+public class PredictableDie
+{
+	public PredictableDie()
+	{
+
+	}
+
+	public PredictableDie(int[] is)
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+	public void roll()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	public int getLastRoll()
+	{
+		
+		return 0;
+	}
+}

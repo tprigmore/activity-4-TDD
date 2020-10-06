9457aa3e74f964583035011461d9d4cf5087c369
Scott Prigmore <prig6030@stthomas.edu>
2020-10-05 19:34:03 -0500

    Initial commit of activity-4-TDD

---
+++ SkunkProject/test/skunk/domain/TestDie.java
@@ -0,0 +1,16 @@
+package skunk.domain;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestDie
+{
+
+	@Test
+	public void test()
+	{
+		fail("Not yet implemented");
+	}
+
+}

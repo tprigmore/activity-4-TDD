6edcb23f98054a0378934d06d7d9e06d0bbae096
Scott Prigmore <prig6030@stthomas.edu>
2020-10-05 20:43:38 -0500

    Green - added exception handling

--- SkunkProject/test/skunk/domain/TestPredictableDie.java
+++ SkunkProject/test/skunk/domain/TestPredictableDie.java
@@ -3,6 +3,7 @@ package skunk.domain;
 import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class TestPredictableDie
@@ -11,33 +12,38 @@ class TestPredictableDie
 	@Test
 	void test_PD_1_2_3()
 	{
-		PredictableDie die = new PredictableDie(new int[] {1,2,3});
+		PredictableDie die = new PredictableDie(new int[]
+		{ 1, 2, 3 });
 		die.roll();
-		assertEquals(1,die.getLastRoll());
+		assertEquals(1, die.getLastRoll());
 		die.roll();
-		assertEquals(2,die.getLastRoll());
+		assertEquals(2, die.getLastRoll());
 		die.roll();
-		assertEquals(3,die.getLastRoll());
+		assertEquals(3, die.getLastRoll());
 		die.roll();
-		assertEquals(1,die.getLastRoll());
+		assertEquals(1, die.getLastRoll());
 	}
-	
+
 	@Test
 	void test_PD_1_more_than_once()
 	{
-		PredictableDie die = new PredictableDie(new int[] {1});
+		PredictableDie die = new PredictableDie(new int[]
+		{ 1 });
 		die.roll();
-		assertEquals(1,die.getLastRoll());
+		assertEquals(1, die.getLastRoll());
 		die.roll();
-		assertEquals(1,die.getLastRoll());
+		assertEquals(1, die.getLastRoll());
 
 	}
 
 	@Test
 	void test_PD_with_empty_initial_int_array()
 	{
-		PredictableDie die = new PredictableDie(new int[] {});
-		die.roll();
+		Assertions.assertThrows(RuntimeException.class, () ->
+		{
+			PredictableDie die = new PredictableDie(new int[]
+			{});
+			die.roll();
+		});
 	}
-
 }
